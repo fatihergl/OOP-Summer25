3ba4e98ee651645064d699d37bc1fc0cd7685e4a
fatihergl <muratergul2005@gmail.com>
2025-03-13 11:19:15 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,15 +1,33 @@
-
-student1 = {
+students = [
+ {
   first_name : "Fatih Murat"
 last_name : "Ergul"
 index_number : 35239
 nationality : "Turkish"
 starting_date : "2025-03-13"
-courses : "Object-oriented programing" }
+courses : ["Object-oriented programing"] } ,
 
-print(student1['firs_name'])
-print(student1['last_name'])
-print(student1['index_number'])
-print(student1['nationality'])
-print(student1['starting_date'])
-print(student1['courses '])
+
+
+
+
+ {
+    first_name : "akber"
+last_name : "cavus"
+index_number : 1111111
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses :[ "Object-oriented programing"] 
+} ,
+
+
+
+ {
+    first_name : "aslr"
+last_name : "cakss"
+index_number : 222222
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses : ["Object-oriented programing"] }  ]
+
+
