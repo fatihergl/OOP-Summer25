b75e847fc9a85258f9d292fcbc7b7f7d8df845f1
fatihergl <muratergul2005@gmail.com>
2025-03-13 10:57:25 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,1 +1,15 @@
 
+student1 = {
+  first_name : "Fatih Murat"
+last_name : "Ergul"
+index_number : 35239
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses : "Object-oriented programing" }
+
+print(student1['firs_name'])
+print(student1['last_name'])
+print(student1['index_number'])
+print(student1['nationality'])
+print(student1['starting_date'])
+print(student1['courses '])
