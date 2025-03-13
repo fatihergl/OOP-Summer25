d3ad7eaf3de63936254f116fcc0058f4b0d63b80
fatihergl <muratergul2005@gmail.com>
2025-03-13 11:02:55 +0100

    Update student.py

--- student.py
+++ student.py
@@ -1,1 +1,47 @@
 
+student1 = {
+  first_name : "Fatih Murat"
+last_name : "Ergul"
+index_number : 35239
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses : "Object-oriented programing" }
+
+print(student1['firs_name'])
+print(student1['last_name'])
+print(student1['index_number'])
+print(student1['nationality'])
+print(student1['starting_date'])
+print(student1['courses '])
+
+
+
+student2 = {
+    first_name : "akber"
+last_name : "cavus"
+index_number : 1111111
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses : "Object-oriented programing" }
+
+print(student2['firs_name'])
+print(student2['last_name'])
+print(student2['index_number'])
+print(student2['nationality'])
+print(student2['starting_date'])
+print(student2['courses ']) }
+
+student3 = {
+    first_name : "aslr"
+last_name : "cakss"
+index_number : 222222
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses : "Object-oriented programing" }
+
+print(student3['firs_name'])
+print(student3['last_name'])
+print(student3['index_number'])
+print(student3['nationality'])
+print(student3['starting_date'])
+print(student3['courses ']) }
