ccf2e4b47d78785ee704ac657cf7ceae83a4c84c
fatihergl <muratergul2005@gmail.com>
2025-03-13 10:43:35 +0100

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -1,1 +1,13 @@
+first_name = "Fatih Murat"
+last_name = "Ergul"
+index_number = 35239
+nationality = "Turkish"
+starting_date = "2025-03-13"
+courses = ["Object-oriented programing"]
 
+print(f"First Name: {first_name}")
+print(f"Last Name: {last_name}")
+print(f"Index Number: {index_number}")
+print(f"Nationality: {nationality}")
+print(f"Starting Date: {starting_date}")
+print(f"Courses: {', '.join(courses)}")
