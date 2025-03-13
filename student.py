325844a071cd2ace33a0615e794b791ca26fc672
fatihergl <muratergul2005@gmail.com>
2025-03-13 11:24:08 +0100

    Update student.py

--- student.py
+++ student.py
@@ -1,3 +1,35 @@
+students = [
+ {
+  first_name : "Fatih Murat"
+last_name : "Ergul"
+index_number : 35239
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses : ["Object-oriented programing"] } ,
+
+
+
+
+
+ {
+    first_name : "akber"
+last_name : "cavus"
+index_number : 1111111
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses :[ "Object-oriented programing"] 
+} ,
+
+
+
+ {
+    first_name : "aslr"
+last_name : "cakss"
+index_number : 222222
+nationality : "Turkish"
+starting_date : "2025-03-13"
+courses : ["Object-oriented programing"] }  ]
+
 
 def add_student(first_name, last_name, index_number, nationalty, starting_date, courses):
     students.append({"first_name": first_name, "last_name": last_name, "index_number": index_number, "nationalty": nationalty, "startin_date": starting_date, "courses": courses})
